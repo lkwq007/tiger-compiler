8eb939b604ea2b3535e9617f6fc6c287880faf27
Zardinality <asxaqz2@gmail.com>
2017-05-27 23:24:31 +0800

    add testcase, makefile, lex, yacc, almost all copied, fix Tr_subscriptVar

--- tiger-compiler/include/translate.h
+++ tiger-compiler/include/translate.h
@@ -45,6 +45,7 @@ Tr_exp Tr_assignExp(Tr_exp lhs, Tr_exp rhs);
 Tr_exp Tr_simpleVar(Tr_access access, Tr_level level);
 Tr_exp Tr_noExp();
 Tr_exp Tr_fieldVar(Tr_exp record, int index);
+Tr_exp Tr_subscriptVar(Tr_exp array, Tr_exp index);
 
 void Tr_procEntryExit(Tr_level level, Tr_exp body, Tr_accessList formals);
 F_fragList Tr_getResult(void);
