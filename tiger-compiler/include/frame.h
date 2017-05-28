196f866b77515ae72153562b4fe564147e689cea
Death <lkwq007@gmail.com>
2017-05-28 12:52:38 +0800

    Update frame

--- tiger-compiler/include/frame.h
+++ tiger-compiler/include/frame.h
@@ -48,3 +48,7 @@ F_accessList F_formals(F_frame f);
 F_access F_allocLocal(F_frame f, bool escape);
 T_exp F_exp(F_access acc, T_exp framePtr);
 T_exp F_externalCall(string str, T_expList args);
+
+Temp_map F_tempMap;
+
+int F_wordSize = 4;
